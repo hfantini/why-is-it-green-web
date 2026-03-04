47bf69e3daa790e65e02ef63fa1b6d9ee5bbf5ff
Henrique Fantini <henrique.fantini@hotmail.com>
2026-03-04 10:45:00 -0300

    Feat/get environment from variables (#28)
    * feat: provide environment from runtime variables
    
    * feat: version upgrade

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,15 @@
-declare const __APP_VERSION__: string
-declare const __BUILD_NUMBER__: string
-declare const __GIT_SHA__: string
+interface RuntimeConfig {
+  environment: string
+}
+
+declare global {
+  declare const __APP_VERSION__: string
+  declare const __BUILD_NUMBER__: string
+  declare const __GIT_SHA__: string
+
+  interface Window {
+    __RUNTIME_CONFIG__: RuntimeConfig
+  }
+}
+
+export {}
